f49ca48a570d483c379f3674a243e60aa1176577
maya koma <mayakoma@gmail.com>
2023-01-13 13:31:58 +0200

    hook  & is Loading

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,12 @@
 import Search from "../search/Search";
 import React, { useState } from "react";
+import ShowDetails from "./ShowDetails";
 import "./Admin.css";
 
 function Admin() {
   const [usersList, setUsersList] = useState([]);
   const [ordersList, setOrdersList] = useState([]);
+
   return (
     <div>
       <h1 className="Admin_title"> Setting Page</h1>
@@ -14,6 +16,14 @@ function Admin() {
           <Search className="Admin_Search" />
         </div>
       </div>
+      <ShowDetails list={[2, 3, 4, 5]} />
+      <div className="Admin_ordersSearchContenier">
+        <h3 className="Admin_ordersSearchTitle">Search Orders:</h3>
+        <div className="Admin_ordersSearch">
+          <Search className="Admin_Search" />
+        </div>
+      </div>
+      <ShowDetails list={[1, 2, 3, 4]} />
     </div>
   );
 }
